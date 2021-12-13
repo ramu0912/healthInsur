033b0644e12e891bf6a53524d7c078e8732ca27c
Ramu Sura <ramu.sura09@gmail.com>
2021-12-13 16:30:04 +0530

    updated

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 var app = angular.module('myApp',[]);
 app.controller('myCtrl', function($scope, $http){
-    $http.post("https://apiqa.indiacover360.com/MasterService.svc/web/GetCityList");
+alert('HI');
+    $scope.Test="Irshad";
+    $http.post("https://apiqa.indiacover360.com/MasterService.svc/web/GetCityList")
     .then(function(response){
         $scope.getCityList=response.data;
     });
